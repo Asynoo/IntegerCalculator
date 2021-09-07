b8ef793f7337a2a8c7096e6e6d5980d3d8dd22d6
Marco Gabel <marcgabel@live.dk>
2021-09-07 14:28:51 +0200

    Initial commit

---
+++ IntegerCalculator/Calculator.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace IntegerCalculator
+{
+    public class Calculator: IntCalculator
+
+    {
+        public int Result { get; private set; }
+
+        public void Reset()
+        {
+            Result = 0;
+        }
+
+        public void Add(int x)
+        {
+            if (x + Result < int.MaxValue && x + Result > int.MinValue)
+            {
+                Result += x;
+            }
+            else
+            {
+                throw new OverflowException();
+            }
+        }
+
+        public void Subtract(int x)
+        {
+            if (Result - x > int.MinValue && Result - x < int.MaxValue) 
+            {
+                Result -= x;
+            }
+            else
+            {
+                throw new OverflowException();
+            }
+        }
+
+        public void Multiply(int x)
+        {
+            if (x * Result < int.MaxValue && x * Result > int.MinValue)
+            {
+                Result *= x;
+            }
+            else
+            {
+                throw new OverflowException();
+            }
+        }
+
+        public void Divide(int x)
+        {
+            if (x==0)
+            {
+                throw new ArgumentException();
+            }
+            if (Result / x > int.MinValue && Result / x < int.MaxValue)
+            {
+                Result /= x;
+            }
+            else
+            {
+                throw new OverflowException();
+            }
+        }
+
+        public void Modulus(int x)
+        {
+            if (Result % x > int.MinValue && Result % x < int.MaxValue)
+            {
+                Result %= x;
+            }
+            else
+            {
+                throw new OverflowException();
+            }
+        }
+    }
+}
---
+++ IntegerCalculator/IntCalculator.cs
@@ -0,0 +1,19 @@
+﻿namespace IntegerCalculator
+{
+    public interface IntCalculator
+    {
+        int Result { get; }
+
+        void Reset();
+        
+        void Add(int x);
+        
+        void Subtract(int x);
+        
+        void Multiply(int x);
+        
+        void Divide(int x);
+        
+        void Modulus(int x);
+    }
+}
